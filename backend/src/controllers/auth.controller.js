ba60b49f4f23e3043b6fea3123250777fcb6159f
hardik <hardikzode05@gmail.com>
2025-12-25 23:50:44 +0530

    starting backend dev

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export function signup(req,res){
+    res.send("Signup Route");
+}
+
+export function login(req,res){
+    res.send("Login Route");
+}
+
+export function logout(req,res){
+    res.send("Logout Route");
+}
