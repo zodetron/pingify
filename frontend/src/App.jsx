8d1e8e7da50185c38592a96cb69bef0262dab165
hardik <hardikzode05@gmail.com>
2026-01-01 23:01:31 +0530

    onboarding complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,14 +44,22 @@ const App = () => {
           ) : (
             <HomePage />
           )
-        }
-      />
+        }/>
       <Route path="/signup" element ={!isAuthenticated ?<SignupPage/>:<Navigate to="/"/> } />
       <Route path="/login" element ={!isAuthenticated ?<LoginPage/>:<Navigate to="/"/>} />
       <Route path="/notifications" element ={isAuthenticated ?<NotificationsPage/>:<Navigate to="/login"/>} />
       <Route path="/call" element ={isAuthenticated ?<CallPage/>:<Navigate to="/login"/>} />
       <Route path="/chat" element ={isAuthenticated ?<ChatPage/>:<Navigate to="/login"/>} />
-      <Route path="/onboarding" element ={isAuthenticated ?<OnboardingPage/>:<Navigate to="/login"/>} />
+      <Route 
+        path="/onboarding" element ={isAuthenticated ?(
+          !isOnboarded ? (
+            <OnboardingPage/>
+          ) : (
+            <Navigate to="/"/>
+          )
+        ):(
+          <Navigate to="/login"/>
+        )} />
     </Routes>
 
     <Toaster/>
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -10,3 +10,8 @@ export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
 }
+
+export const completeOnboarding = async (userData) =>{
+  const res = await axiosInstance.post("/auth/onboarding",userData);
+  return res.data;
+}
