a54e35ef6794d5cbe78bd2de079a69f9a3afaa22
hardik <hardikzode05@gmail.com>
2026-01-06 01:15:56 +0530

    setting up home page

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -29,3 +29,23 @@ export const completeOnboarding = async (userData) =>{
   const res = await axiosInstance.post("/auth/onboarding",userData);
   return res.data;
 }
+
+export async function getUserFriends(){
+  const response = await axiosInstance.get("/users/friends");
+  return response.data;
+}
+
+export async function getRecommendedUsers(){
+  const response = await axiosInstance.get("/users");
+  return response.data;
+}
+
+export async function getOutgoingFriendReqs(){
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export async function sendFriendRequest(userId){
+  const response = await axiosInstance.post(`user/friend-request/${userId}`);
+  return response.data;
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import React, { useEffect, useState } from 'react'
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from '../lib/api';
+import { UserIcon, UsersIcon } from 'lucide-react';
+import { Link } from 'react-router';
 
 const HomePage = () => {
+  const queryClient = useQueryClient();
+  const [outgoingRequestsIds,setOutgoingRequestsIds] = useState(new Set())
+
+  const{data:friends=[],isLoading:loadingFriends} = useQuery({
+    queryKey:["friends"],
+    queryFn: getUserFriends,
+  })
+
+  const{data:recommendedUsers=[],isLoading:loadingUsers} = useQuery({
+    queryKey:["users"],
+    queryFn: getRecommendedUsers,
+  })
+
+  const{data:outgoingFriendReqs} = useQuery({
+    queryKey:["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  })
+
+  const {mutate:sendRequestMutation, isPending} = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess:() => queryClient.invalidateQueries({ queryKey:["outgoingFriendReqs"]}),
+  });
+
+  useEffect(()=>{
+    const outgoingIds = new Set()
+    if(outgoingFriendReqs && outgoingFriendReqs.length>0){
+      outgoingFriendReqs.forEach((req)=>{
+        outgoingIds.add(req.id)
+      })
+      setOutgoingRequestsIds(outgoingIds)
+    }
+  },[outgoingFriendReqs])
+
   return (
-    <div>
-      HomePage hlowwwww
+    <div className='p-4 sm:p-6 lg:p-8'>
+      <div className='container mx-auto space-y-10'>
+        <div className='flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4'>
+          <h2 className='text-2xl sm:text-3xl font-bold tracking-tight'>Your Friends</h2>
+          <Link to="/notifications" className='btn btn-outline btn-sm'>
+              <UsersIcon className='mr-2 size-4'/>
+              Friend Requests
+          </Link>
+        </div>
+      </div>  
     </div>
-  )
-}
+  );
+} 
 
 export default HomePage
