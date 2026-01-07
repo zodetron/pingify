f79894e68f7fc455a2bb5c1e50faeb2eb2c6b96b
hardik <hardikzode05@gmail.com>
2026-01-07 14:42:35 +0530

    accept req page routing

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
 import { acceptFriendRequest, getFriendRequests } from '../lib/api';
+import {UserCheckIcon} from 'lucide-react'
 
 const NotificationsPage = () => {
   const queryClient = useQueryClient();
@@ -32,7 +33,52 @@ const acceptedRequests = friendRequests?.acceptedRequests || []
           </div>
         ) : (
           <>
-          
+            {incomingRequests.length>0 && (
+              <section className='space-y-4'>
+                <h2 className='text-xl font-semibold flex items-center gap-2'>
+                  <UserCheckIcon/>
+                  Friend Requests
+                  <span className='badge badge-primary ml-2'>{incomingRequests.length}</span>
+                  </h2>
+
+                <div className='space-y-3'> 
+                  {incomingRequests.map((request) =>(
+                    <div
+                      key={request._id}
+                      className='card bg-base-200 shadow-sm hover:shadow-md transition-shadow'
+                      >
+                      <div className='card-body p-4'>
+                        <div className='flex items-center justify-between'>
+                          <div className='flex items-center gap-3'>
+                            <div className='avatar w-14 h-14 rounded-full bg-base-300'>
+                              <img src={request.sender.profilePic} alt={request.sender.fullName} />
+                            </div>
+                          </div>
+                            <h3 className='font-semibold'>{request.sender.fullName}</h3>
+                            <div className='flex flex-wrap gap-1.5 mt-1'>
+                              <span className='badge badge-secondary badge'>
+                                Native: {request.sender.nativeLanguage} 
+                              </span>
+                              <span className='badge badge-outline badge'>
+                                Learning: {request.sender.learningLanguage} 
+                              </span>
+                            </div>
+                        </div>
+
+                      <button
+                        className='btn btn-primary btn-sm'
+                        onClick={()=> acceptRequestMutation(request._id)}
+                        disabled={isPending}
+                        >
+                        Accept
+                      </button>
+                        </div>
+                    </div>
+                  ))}
+                </div>
+
+              </section>
+            )}
           </>
         )}
       </div>
