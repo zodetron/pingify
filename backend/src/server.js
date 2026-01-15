5a1d4eade31e0fac80294957a2a3a9f0e40a85ab
hardik <hardikzode05@gmail.com>
2026-01-15 18:33:10 +0530

    made prod ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT
+
+const  __dirname = path.resolve();
 
 // app.get("/api/auth/signup",(req,res)=>{
 //     res.send("Signup Route");
@@ -34,6 +39,15 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectDB();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
   withCredentials: true, // 🔴 REQUIRED
 });
